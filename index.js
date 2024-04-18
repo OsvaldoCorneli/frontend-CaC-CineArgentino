1aec48719b2717a026217c7a997c95965fe3dd62
Osvaldo Matias Corneli Nassif <122895353+OsvaldoCorneli@users.noreply.github.com>
2024-04-18 20:43:56 -0300

    agregue el modal faltaria modificarlo y agregar descripcion

---
+++ index.js
@@ -0,0 +1,15 @@
+
+    console.log("hola")
+
+
+function openModal(id){
+    const boton = document.querySelector("#botonModals")
+    document.querySelector("#exampleModalLabel").textContent = "andrea del boca"
+    document.querySelector(".modal-body").innerText = "ESTE ES UN EJEMPLO DE LA DESCRIPCION DE ANDREA DEL BOCA"
+
+
+
+    boton.click()
+
+
+}
