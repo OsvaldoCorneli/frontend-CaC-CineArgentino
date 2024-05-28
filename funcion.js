549adf2e20b2a42a5c942adae46ef60ad9bdad19
OsvaldoCorneli <overcorneli@gmail.com>
2024-05-28 10:43:23 -0300

    separe las vistas en distintos html

--- funcion.js
+++ funcion.js
@@ -1,47 +1,49 @@
 
-function navegar(direccion){
-    const botonMenu = document.querySelector(".navbar-toggler") 
-    let destino = 0
-    switch(direccion){
-        case "inicio":
-            destino = document.getElementById("inicio").offsetTop; 
-        break;
-        case "estreno":
-            destino = document.getElementById("estreno").offsetTop; 
-        break;
-        case "actrices":
-            destino = document.getElementById("actrices").offsetTop;      
-        break;
-        case "actores":
-            destino = document.getElementById("actores").offsetTop;   
-        break;
-        case "directores":
-            destino = document.getElementById("directores").offsetTop;  
-        break;
-        case "clasica":
-            destino = document.getElementById("clasica").offsetTop;  
-        break;
 
-        default:
-        break;
-    }
+// function navegar(direccion){
+//     const botonMenu = document.querySelector(".navbar-toggler") 
+//     let destino = 0
+//     switch(direccion){
+//         case "inicio":
+//             destino = document.getElementById("inicio").offsetTop; 
+//         break;
+//         case "estreno":
+//             destino = document.getElementById("estreno").offsetTop; 
+//         break;
+//         case "actrices":
+//             destino = document.getElementById("actrices").offsetTop;      
+//         break;
+//         case "actores":
+//             destino = document.getElementById("actores").offsetTop;   
+//         break;
+//         case "directores":
+//             destino = document.getElementById("directores").offsetTop;  
+//         break;
+//         case "clasica":
+//             destino = document.getElementById("clasica").offsetTop;  
+//         break;
 
-    window.scrollTo({
-        top: destino,
-        behavior: 'smooth'
-    });
+//         default:
+//         break;
+//     }
 
-    if(botonMenu.ariaExpanded){
-        botonMenu.click()
-    }
+//     window.scrollTo({
+//         top: destino,
+//         behavior: 'smooth'
+//     });
 
-}
+//     if(botonMenu.ariaExpanded){
+//         botonMenu.click()
+//     }
+
+// }
 
 
-function deleteRouteLink(){
+// function deleteRouteLink(){
     
-    if (window.location.hash) {
-      history.replaceState({}, document.title, window.location.href.split('#')[0]);
-  }}
+//     if (window.location.hash) {
+//       history.replaceState({}, document.title, window.location.href.split('#')[0]);
+//   }}
 
 
+    
