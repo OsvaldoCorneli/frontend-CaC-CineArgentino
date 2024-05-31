ccd624ff0a6e41faf10a29f1fd250d085de3942a
OsvaldoCorneli <overcorneli@gmail.com>
2024-05-31 14:06:02 -0300

    agregue json peliculas y funcionabilidad

--- public/script/peliculas.js
+++ public/script/peliculas.js
@@ -1,5 +1,5 @@
 const url_peliculas = "./public/datos/peliculas.json"
-
+let estrenos = []
 let peliculas; 
 const getPeliculas =  async ()=>{
 
@@ -18,36 +18,79 @@ async function htmlArmado(){
 
     peliculas.forEach((pelicula)=>{
 
-        console.log()
-        if(pelicula.estreno.split("-")[0] != "2024"){ 
+        if(pelicula.estreno.split("-")[0] != "2024" && pelicula.estreno.split("-")[0] != "2023"){ 
         const divs = document.createElement("div")
         divs.classList.add("col-sm-12", "col-md-6", "col-lg-4", "col-xxl-3", "text-center")
         
         divs.innerHTML = `
-        <img src="${pelicula.imagen}"  alt="${pelicula.tittle}">
+        <a onclick='popUpPelicula(${pelicula.id})' style="cursor: pointer"><img src="${pelicula.imagen}"  alt="${pelicula.tittle}" ></a>
         `
     
         row.appendChild(divs)
     }
     else{
-        console.log(pelicula.estreno)
+        estrenos.push(pelicula)
     }
 
     })
-    
-}
 
+    if(estrenos.length > 0){
+
+            const wraper = document.querySelector(".wrapper")
+        
+            estrenos.forEach((pelicula)=>{
+                const divs = document.createElement("div")
+                divs.classList.add("item-image")
+                
+                divs.innerHTML = `
+                <a onclick='popUpPelicula(${pelicula.id})'><img src=${pelicula.imagen} alt="${pelicula.title}"></a><h6 class="text-white text-center">${pelicula.title}</h6>
+                `
+            
+                wraper.appendChild(divs)
+        
+        
+            })
+            
+        }
+
+
+
+    }
+    
 htmlArmado()
 
-function popUpActriz(id){
+function popUpPelicula(id){
+        console.log("LA PELICULA id", id)
+    const boton = document.querySelector("#botonPeliculaModals")
     
-    const boton = document.querySelector("#botonModals")
-    
-            actrices.forEach((element)=>{
-            if(element.id == id){
-                    document.querySelector("#exampleModalLabel").textContent = element.name
-                    document.querySelector(".modal-body").innerText = element.description
-                    document.querySelector("#wikiModal").href = element.wikilink
+            peliculas.forEach((element)=>{
+                
+            if(element.id == id){            
+            const titulo = document.querySelector("#staticBackdropLabel")
+            const informacion = document.querySelector("#informacion")
+            const imagen = document.querySelector("#imagenPelicula")
+                
+            titulo.textContent = element.title
+            informacion.innerHTML = `
+
+                <h5><b>Sinopsis: </b> ${element.sinopsis}</h5>
+                <br>
+                <h5><b>Elenco: </b> ${element.elenco}</h5>
+                <br>
+                <h5><b>Director: </b> ${element.director}</h5>
+                <br>
+                <h5><b>Duracion: </b> ${element.duracion}</h5>
+                <br>
+                <h5><b>Estreno:</b> ${element.estreno}</h5>
+                <br>
+
+
+            `
+
+            imagen.src = element.imagen
+
+            console.log(informacion)
+
             }
         })
          boton.click()
